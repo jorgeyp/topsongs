2a5e82e8418e60e1c32b611a263f11462298c60a
Alumno <puente@Mac-de-Jorge-2.local>
2012-10-10 13:22:31 +0200

    Initial Commit

---
+++ topsongs/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  topsongs
+//
+//  Created by Jorge Puente Peinador on 10/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ topsongs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  topsongs
+//
+//  Created by Jorge Puente Peinador on 10/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
